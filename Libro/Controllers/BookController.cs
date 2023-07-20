27ea6302a0b51ee181f6cfa9128c227f92501882
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-20 15:58:18 +0300

    BookBrwosing Apis

---
+++ Libro/Controllers/BookController.cs
@@ -0,0 +1,50 @@
+﻿using Libro.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            List<Book> books = _bookRepository.GetAllBooks();
+            return Ok(books);
+        }
+
+        [HttpGet("available")]
+        public IActionResult GetAvailableBooks()
+        {
+            List<Book> availableBooks = _bookRepository
+                .GetAllBooks()
+                .Where(b => b.AvailabilityStatus)
+                .ToList();
+
+            return Ok(availableBooks);
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value) { }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
---
+++ Libro/Program.cs
@@ -0,0 +1,39 @@
+using Libro.Data.Mappers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IApplicationBuilder, ApplicationBuilder>();
+builder.Services.AddScoped<IBookRepository, BookMockRepository>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System
+            .Text
+            .Json
+            .Serialization
+            .ReferenceHandler
+            .Preserve;
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
