8c1e542167c577137844c030d0f6df8eb52ebdc0
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-20 15:58:58 +0300

    Dtos And Mappers ADDED

---
+++ Libro.Data/DTOs/BookSearchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Libro.Data.DTOs
+{
+    public class BookDTO
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; } = string.Empty;
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
