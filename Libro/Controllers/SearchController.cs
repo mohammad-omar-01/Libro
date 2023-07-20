174347b15acdf7de49d1bfdca7f02041d8dd7479
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-20 16:00:07 +0300

    Searching For A book API

---
+++ Libro/Controllers/SearchController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Libro.Data.DTOs;
+using Libro.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SearchController : ControllerBase
+{
+    private readonly IBookRepository bookRepository;
+    private readonly IMapper _mapper;
+
+    public SearchController(IBookRepository bookRepository, IMapper mapper)
+    {
+        this.bookRepository = bookRepository;
+        this._mapper = mapper;
+    }
+
+    [HttpGet("books")]
+    public IActionResult SearchBooks(string query = null)
+    {
+        List<Book> books = bookRepository.GetAllBooks();
+
+        if (!string.IsNullOrWhiteSpace(query))
+        {
+            books = books
+                .Where(
+                    b =>
+                        b.Title.Contains(query, StringComparison.OrdinalIgnoreCase)
+                        || b.Authors.Any(
+                            a => a.Name.Contains(query, StringComparison.OrdinalIgnoreCase)
+                        )
+                        || b.Genre.Contains(query, StringComparison.OrdinalIgnoreCase)
+                )
+                .ToList();
+
+            return Ok(books);
+        }
+        List<BookDTO> bookDTOs = _mapper.Map<List<BookDTO>>(books);
+
+        return Ok(bookDTOs);
+    }
+}
