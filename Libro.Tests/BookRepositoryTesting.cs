1d9cfb684c695d8cb272c48f6de33f65948e9b19
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-20 15:53:31 +0300

    BookRepostiory Tests

---
+++ Libro.Tests/BookRepositoryTesting.cs
@@ -0,0 +1,68 @@
+using Libro.Data.Models;
+
+public class BookMockRepositoryTests
+{
+    private BookMockRepository bookRepository;
+
+    public BookMockRepositoryTests()
+    {
+        bookRepository = new BookMockRepository();
+    }
+
+    [Fact]
+    public void AddBook_ShouldIncrementBookIdCounter()
+    {
+        int initialCounter = bookRepository.GetAllBooks().Count;
+        Book newBook = new Book
+        {
+            Title = "Test Book",
+            ISBN = "1234567890",
+            Genre = "Test Genre",
+            PublicationDate = DateTime.Now,
+            AvailabilityStatus = true
+        };
+
+        bookRepository.AddBook(newBook);
+
+        int finalCounter = bookRepository.GetAllBooks().Count;
+        Assert.Equal(initialCounter + 1, finalCounter);
+    }
+
+    [Fact]
+    public void UpdateBook_ShouldUpdateBookProperties()
+    {
+        Book existingBook = bookRepository.GetBookById(1);
+        if (existingBook != null)
+        {
+            Book updatedBook = new Book
+            {
+                BookID = existingBook.BookID,
+                Title = "Updated Title",
+                ISBN = "0987654321",
+                Genre = "Updated Genre",
+                PublicationDate = existingBook.PublicationDate,
+                AvailabilityStatus = existingBook.AvailabilityStatus
+            };
+
+            bookRepository.UpdateBook(updatedBook);
+
+            Book updatedBookFromRepository = bookRepository.GetBookById(existingBook.BookID);
+            Assert.Equal("Updated Title", updatedBookFromRepository.Title);
+            Assert.Equal("0987654321", updatedBookFromRepository.ISBN);
+            Assert.Equal("Updated Genre", updatedBookFromRepository.Genre);
+        }
+    }
+
+    [Fact]
+    public void DeleteBook_ShouldRemoveBookFromRepository()
+    {
+        int initialCounter = bookRepository.GetAllBooks().Count;
+        int bookIdToDelete = 1;
+
+        bookRepository.DeleteBook(bookIdToDelete);
+
+        int finalCounter = bookRepository.GetAllBooks().Count;
+        Assert.Equal(initialCounter - 1, finalCounter);
+        Assert.Null(bookRepository.GetBookById(bookIdToDelete));
+    }
+}
