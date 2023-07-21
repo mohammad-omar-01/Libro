392f8329dde96d335858668ad0607ec2ddbc876f
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-21 14:18:55 +0300

    Update IbookInterface for Adding Copy

--- Libro.Data/Repos/IBookRepository.cs
+++ Libro.Data/Repos/IBookRepository.cs
@@ -7,4 +7,6 @@ public interface IBookRepository
     List<Book> GetAllBooks();
     Book GetBookById(int bookId);
     void UpdateBook(Book updatedBook);
+    void AddBookCopy(int bookId, bool isAvailable)
+
 }
