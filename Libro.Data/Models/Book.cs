b0112d822a4e3db9f53ef1b6a9d0efdf455b80ac
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-21 14:03:14 +0300

    Book Copy is created

--- Libro.Data/Models/Book.cs
+++ Libro.Data/Models/Book.cs
@@ -9,12 +9,18 @@
         public DateTime PublicationDate { get; set; }
         public bool AvailabilityStatus { get; set; }
         public bool IsReserved { get; set; }
-
         public List<Author> Authors { get; set; }
+        public List<BookCopy> Copies { get; set; }
 
         public Book()
         {
+            Copies = new List<BookCopy>();
             Authors = new List<Author>();
+        }
+
+        public bool AnyCopiesAvailable()
+        {
+            return Copies.Any(copy => copy.IsAvailable);
         }
     }
 }
