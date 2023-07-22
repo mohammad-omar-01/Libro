9fe5e46b71effb546f5e48c9ebb3204e7d668573
MohammadOmar <mohammad.m.omar.01@gmail.com>
2023-07-22 10:44:41 +0300

    Admin Assign Roles is built

---
+++ Libro.Data/Repos/AdminRepository.cs
@@ -0,0 +1,15 @@
+﻿using Libro.Data.Models;
+
+namespace Libro.Data.Repos
+{
+    public class AdminRepository : IAdminRepository
+    {
+        public AdminRepository() { }
+
+        public User AssignUserRole(User user, UserRole role)
+        {
+            user.Role = role;
+            return user;
+        }
+    }
+}
---
+++ Libro/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Libro.Data.Models;
+using Libro.Data.Repos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Libro.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Policy = "MustBeAdmin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminRepository _adminRepository;
+        private readonly IUserRepository _userRepository;
+
+        public AdminController(IAdminRepository adminRepository, IUserRepository userRepository)
+        {
+            this._adminRepository = adminRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, string role)
+        {
+            var user = _userRepository.GetUserById(id);
+            if (user == null)
+            {
+                return BadRequest("User Not Found");
+            }
+            UserRole userRole;
+            Enum.TryParse<UserRole>(role, out userRole);
+            user = _adminRepository.AssignUserRole(user, userRole);
+            return Ok(user);
+        }
+    }
+}
